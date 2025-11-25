c5e514e1b0dfd655e2abfaeb71711f5e5c6e6081
Gilbertoknd <gilbertoknd5@gmail.com>
2025-11-25 10:41:49 -0300

    Conexão entre o postgresql e o backend; docker compose

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { pool } from "./database/db";
 
 dotenv.config();
 
@@ -10,12 +11,28 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({
-    mensagem: "Backend do Maid Café Mew Mew!",
-    status: "Positivo e operante 🚀",
-  });
+//Rota de Teste do Banco
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW() as hora_atual");
+
+    res.json({
+      status: "Sucesso",
+      mensagem: "Backend conectado ao PostgreSQL!",
+      horario_banco: result.rows[0].hora_atual,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Falha ao conectar no banco" });
+  }
 });
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.json({
+//     mensagem: "Backend do Maid Café Mew Mew!",
+//     status: "Positivo e operante 🚀",
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`☕ Servidor rodando na porta ${PORT}`);
